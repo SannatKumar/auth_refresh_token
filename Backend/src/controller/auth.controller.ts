0a9a53c686e41a30b09ac10e303482e764f740ed
raj <interplaytheheads@gmail.com>
2023-04-01 22:18:29 +0300

    Password do not match implemented

--- Backend/src/controller/auth.controller.ts
+++ Backend/src/controller/auth.controller.ts
@@ -5,6 +5,12 @@ export const Register = (req: Request, res: Response) =>{
     //Get the body from Postman Post Request
     const body = req.body;
 
+    //Check whether password adn password confirm are same
+    if(body.password !== body.password_confirm){
+        res.status(400).send({
+            message: "Password do not match!"
+        });
+    }
     //Send the response Body back as it is for testing.
     res.send(body);
 }
