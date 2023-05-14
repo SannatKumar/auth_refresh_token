e469e785e9ff0e6f51f1c4127c54005e3bf7a02d
raj <interplaytheheads@gmail.com>
2023-05-14 20:24:21 +0300

    Home and Nav Added to routes

--- react-auth/src/App.tsx
+++ react-auth/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Login } from './componenets/Login';
 import { Register } from './componenets/Register';
 import { Nav } from './componenets/Nav';
+import { Home } from './componenets/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Nav />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register/>} />
       </Routes>  
--- react-auth/src/componenets/Nav.tsx
+++ react-auth/src/componenets/Nav.tsx
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom";
 export const Nav = () => {
     return (
         <header className="p-3 text-bg-dark">
             <div className="container">
                 <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                     <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                        <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
+                        <li><Link to="/" className="nav-link px-2 text-secondary">Home</Link></li>
                     </ul>
                     <div className="text-end">
-                        <button type="button" className="btn btn-outline-light me-2">Login</button>
-                        <button type="button" className="btn btn-warning">Register</button>
+                        <Link to="/login" className="btn btn-outline-light me-2">Login</Link>
+                        <Link to="/register" className="btn btn-outline-light me-2">Register</Link>
                     </div>
                 </div>
             </div>
