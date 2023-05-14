7da2d9148f11b72eefc9f8da3a117d07ebc72235
raj <interplaytheheads@gmail.com>
2023-05-14 20:18:48 +0300

    Navigation added

--- react-auth/src/App.tsx
+++ react-auth/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Login } from './componenets/Login';
 import { Register } from './componenets/Register';
+import { Nav } from './componenets/Nav';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register/>} />
